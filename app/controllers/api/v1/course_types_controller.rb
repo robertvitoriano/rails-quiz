dbf72c92f17cb67a749ab842dc556e1533a204a2
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-19 20:54:35 -0300

    add course type creation route

--- app/controllers/api/v1/course_types_controller.rb
+++ app/controllers/api/v1/course_types_controller.rb
@@ -2,8 +2,21 @@ module Api
   module V1 
    class CourseTypesController < ApplicationController 
       def index
-        course_types = CourseType.order('created_at DESC')
-        render json: {status:'SUCCESS', message:'Loaded course types', data:course_types}, status: :ok
+        course_types = CourseTypes.order('created_at DESC')
+        render json: {status:'SUCCESS', message:'Loaded courses', data:course_types}, status: :ok
+      end
+
+      def create
+        course_types = CourseType.create(course_types_params)
+        if course_types.save
+          render json: {status:'Saved Course Type', message:'saved the course type', data: course_types}, status: :ok
+        else
+          render json: {status:'Not saved', message:'couldn"t save course type'}, status: :ok
+        end
+      end
+
+      def course_types_params
+        params.permit(:title)
       end
     end
   end
