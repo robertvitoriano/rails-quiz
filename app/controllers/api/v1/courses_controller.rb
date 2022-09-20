55783cb326901eae1ce4a30dad35d8bb124fc328
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-20 19:17:14 -0300

    #9788 fix: ajusta estilo do botão de resposta ao salvar drop

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -3,7 +3,7 @@ module Api
     class CoursesController < ApplicationController
       def index
         courses = Course.order('created_at DESC')
-        render json: {status:'SUCCESS', message:'Loaded courses', data:courses}, status: :ok
+        render json: {status:'SUCCESS', message:'Loaded courses', data:params}, status: :ok
       end
 
       def show
@@ -22,17 +22,46 @@ module Api
       end
 
       def create
-        begin  # "try" block7
-          course = Course.create(course_params)
+        begin  
+          course = Course.create({
+             :title => course_params[:title],
+             :goal => course_params[:goal],
+             :course_type_id => course_params[:course_type_id]
+            })
           course.save!
-          render json: {status:'Saved Course', message:'saved the course', data: course}, status: :ok
+          questions = []
+          for q in course_params[:questions] do
+            course_question = CourseQuestion.create(
+              {
+              :question_text => q[:question_text],
+              :course_id => course[:id]
+              }
+            )
+            course_question.save!
+            questions.push(course_question)
+          end
+
+          render json: {
+                        status:'Saved Course',
+                        message:'saved the course',
+                        data:{:course => course,
+                              :questions => questions
+                         }
+                      },
+                     status: :ok
           rescue  Exception => ex
           render json: {status:'Not saved', message:ex}, status: :bad_request
           end 
         end
 
       def course_params
-        params.permit(:title, :goal, :course_types_id)
+        params.permit(
+           :title,
+           :goal,
+           :course_type_id,
+           questions:[:question_text, question_alternatives:[:is_right, :alternative_text]]
+            )
+
       end
     end
   end
