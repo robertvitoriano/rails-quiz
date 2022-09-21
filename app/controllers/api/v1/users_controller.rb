4fbd41219177a26ffb599b8f072b20d3edc28c24
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-21 07:37:16 -0300

    add login route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,45 @@
 module Api
   module V1
+  class UsersController < ApplicationController
+      def create 
+        begin
+        user = User.create(create_user_params)
+        user.save!
+          render json: {status:'SUCCESS', message:'created user', data:user}, status: :ok
 
-    def create 
-      
-      user = User.create()
+        rescue  Exception => ex
+          render json: {status:'Not saved', message:ex}, status: :bad_request
+        end 
+      end
+
+      def login
+        user = User.find_by(username: params['username'])
+        isAuthed = user.try(:authenticate, params['password'])
+
+        if !user
+            render json: {
+                key: 'username',
+                message: 'No user can be found with that Username'
+            }, status: :forbidden
+        elsif !isAuthed
+            render json: {
+                key: 'password',
+                message: 'Incorrect Password'
+                }, status: :forbidden
+        else
+            render json: {
+                id: user.id,
+                username: user.username,
+            }
+        end
     end
 
-    def create_user_params
-      params.permit(:name, :username, :email, :password)
+      def create_user_params
+        params.permit(:name, :username, :email, :password)
+      end
+      def login_user_params
+        params.permit(:username,:password)
+      end
     end
-    
-  end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :courses, :course_types, :course_questions, :question_alternatives, :users
+      resources :courses, :course_types, :course_questions, :question_alternatives
+      resources :users do
+        collection do
+          post :login
+        end
+      end
     end
   end
 end
