fc535b80baf9dc885e81518bccde6605f538b0ac
robertvitoriano <robertvitoriano@gmail.com>
2022-10-29 19:13:42 -0300

    fix route save users answers

--- app/controllers/api/v1/question_alternatives_controller.rb
+++ app/controllers/api/v1/question_alternatives_controller.rb
@@ -1,18 +1,25 @@
 module Api
   module V1
     class QuestionAlternativesController < ApplicationController
+      before_action :authenticate_user_request, only: [:save_user_answer]
+
       def save_user_answer
         begin
           user_answer = UserAlternative.create({
             :user_id => current_user_id,
-            :question_alternative_id => params['question_alternative_id']
+            :question_alternative_id => save_user_answer_params['questionAlternativeId']
           })
           user_answer.save!
           render json: {status:'SUCCESS', message:'saved user answer', data: user_answer}, status: :ok
-        rescue  Exception => ex 
+        rescue  Exception => ex
           render json: {status:'Not saved', message:ex}, status: :ok
         end
       end
+
+     def save_user_answer_params
+      params.permit(:questionAlternativeId)
+     end
+
     end
   end
 end
