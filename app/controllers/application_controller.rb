66fa0766a0a3caf216dc78c8805a6cc16ac8936b
robertvitoriano <robertvitoriano@gmail.com>
2022-10-28 21:38:28 -0300

    adjust image upload to adapt to format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,13 +40,15 @@ class ApplicationController < ActionController::API
   def upload_to_s3(s3_client, object_key, request_file)
     @tmp_path = "#{Rails.root}/tmp/storage/course_covers"
     write_file_to_storage(request_file, @tmp_path)
+    splitted_file_name = course_params[:cover].original_filename.split('.', 2)
+    file_format = splitted_file_name[1]
     File.open(@tmp_path+'/'+course_params[:cover].original_filename, 'rb') do |file|
       upload_to_s3(s3_client, object_key, file)
       s3_client.put_object(
         bucket: ENV["S3_BUCKET"],
         key: object_key,
         acl:'public-read',
-        content_type:'image/jpeg',
+        content_type:'image/'+file_format,
         content_disposition:'attachment',
         body: file
       )
