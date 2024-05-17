1074a3036121736fdbcafaba701d67d7ac18e127
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-05-18 01:02:24 +0900

    fix syntax error in notification service

--- app/services/NotificationService.rb
+++ app/services/NotificationService.rb
@@ -1,19 +1,18 @@
-
 module Api
   module V1
     class NotificationService
       def self.create_notification(notifier_id, notified_id, type_id)
         notification = user.notifications.create(
-                                                  notification_type_id: type_id,
-                                                  is_read: false,
-                                                  notifier_id: notifier_id,
-                                                  notified_id: notified_id
-                                                )
+          notification_type_id: type_id,
+          is_read: false,
+          notifier_id: notifier_id,
+          notified_id: notified_id
+        )
         
         notification.save!
         
         notification
-    
       end
+    end
   end
 end
